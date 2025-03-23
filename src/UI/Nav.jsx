f99b5eb82233c1b05daa715b5d845fb140f678a4
Truella <turahsuleiman1212@gmail.com>
2025-03-23 23:08:57 +0100

    fixed path

--- src/UI/Nav.jsx
+++ src/UI/Nav.jsx
@@ -3,11 +3,11 @@ import NavLink from "./Link";
 
 export default function Nav({navIsOpen}) {
   const navLinks = [
-    { id: 1, value: "Home", path: "/" },
-    { id: 2, value: "Menu", path: "/menu" },
-    { id: 3, value: "Order Online", path: "/order-online" },
-    { id: 4, value: "Make a Reservation", path: "/reservation" },
-    { id: 5, value: "Contact Us", path: "/contact" },
+    { id: 1, value: "Home", path: "./" },
+    { id: 2, value: "Menu", path: "./menu" },
+    { id: 3, value: "Order Online", path: "./order-online" },
+    { id: 4, value: "Make a Reservation", path: "./reservation" },
+    { id: 5, value: "Contact Us", path: "./contact" },
   ];
   return (
     <nav className={`${!navIsOpen ? "h-0 opacity-0" : "h-[45vh] opacity-100"} overflow-hidden transition-all duration-500 ease-in-out md:flex md:w-[75vw] md:justify-between md:opacity-100 md:h-auto `}>
